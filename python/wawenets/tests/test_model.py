0a4cc23578710990244038ea9a24af32a7613373
Andrew Catellier <acatellier@ntia.doc.gov>
2022-07-31 20:20:01 -0600

    add vanilla pytorch model files, update tests

--- python/wawenets/tests/test_model.py
+++ python/wawenets/tests/test_model.py
@@ -8,12 +8,17 @@ from wawenets.model import WAWEnet2022
 
 @pytest.fixture
 def model_weights_2020():
-    return resource_filename(
-        "weights", "20200801_WAWEnetFD13FC96AvgReLU_PESQMOSLQO_final_pytorch_eval.pt",
-    )
+    weights_paths = [
+        resource_filename("weights", "20200801_WAWEnetFD13FC96AvgReLU_PESQMOSLQO.pt"),
+        resource_filename("weights", "20200801_WAWEnetFD13FC96AvgReLU_POLQAMOSLQO_.pt"),
+        resource_filename("weights", "20200802_WAWEnetFD13FC96AvgReLU_PEMO.pt"),
+        resource_filename("weights", "20200802_WAWEnetFD13FC96AvgReLU_STOI.pt"),
+    ]
+    return weights_paths
 
 
 def test_WAWEnet2022(model_weights_2020):
-    script_module = torch.jit.load(model_weights_2020)
-    model = WAWEnet2022()
-    model.load_state_dict(script_module.state_dict())
+    for weight_path in model_weights_2020:
+        script_module = torch.load(weight_path)
+        model = WAWEnet2022()
+        model.load_state_dict(script_module["model_state_dict"])
