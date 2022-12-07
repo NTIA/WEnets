72f3cd43c718f4e4025e354d88445baa4566da91
Andrew Catellier <acatellier@ntia.doc.gov>
2022-12-07 09:16:04 -0700

    some cleanup on the training script

--- python/wawenets/wawenet_trainer/train.py
+++ python/wawenets/wawenet_trainer/train.py
@@ -1,6 +1,8 @@
 import argparse
 import importlib
 
+from typing import Any, List
+
 from clearml import Task
 
 import pytorch_lightning as pl
@@ -27,6 +29,121 @@ def get_normalizer_class(target, bw="wb", norm_ind=0):
     return instance
 
 
+def train(
+    project_name: str = "",
+    dataset_name: str = "",
+    batch_size: int = None,
+    training_epochs: int = None,
+    seed: int = None,
+    pred_metric: List[str] = "",
+    groups_json: str = "",
+    data_fraction: float = None,
+    initial_weights: str = None,
+    unfrozen_layers: int = None,
+    segments: List[str] = "",
+    csv_path: str = "",
+    data_root_path: str = "",
+    test_only: bool = False,
+    lightning_module_name: str = "",
+    initial_learning_rate: float = None,
+    channels: int = None,
+    num_workers: int = None,
+):
+    target_list = " ".join(pred_metric)
+    # TODO: is this fixed yet?
+    Task.add_requirements("setuptools", "59.5.0")
+    task = Task.init(
+        project_name=project_name,
+        task_name=f"{target_list}_from_script",
+        output_uri="/home/whltexbread/ml_results",
+    )
+    # TODO: we've gotta pass the task around so we can upload artifacts
+    #       and some reporting too.
+
+    # TODO: correctly specify `worker_init_fn`
+    # TODO: incorporate num workers
+    pl.seed_everything(seed)
+
+    normalizers = [
+        get_normalizer_class(metric, norm_ind=ind)
+        for ind, metric in enumerate(pred_metric)
+    ]
+
+    # set up the dataset
+    speech_transforms = [
+        NormalizeAudio(),
+        AudioToTensor(),
+    ]
+    speech_transforms.extend(normalizers)
+
+    augment_transforms = [
+        NormalizeAudio(),
+        InvertAudioPhase(),
+        AudioToTensor(),
+    ]
+    augment_transforms.extend(normalizers)
+
+    speech_transforms = transforms.Compose(speech_transforms)
+    augment_transforms = transforms.Compose(augment_transforms)
+
+    transform_list = [speech_transforms, augment_transforms]
+    dataset_class = get_class(dataset_name, "wawenet_trainer.lightning_data")
+
+    if segments:
+        segments = segments
+    else:
+        segments = ["seg_1"]
+
+    fr_target_names = {
+        "PESQMOSLQO",
+        "POLQAMOSLQO",
+        "PEMO",
+        "ViSQOL3_C310",
+        "STOI",
+        "ESTOI",
+        "SIIBGauss",
+    }
+    match_segments = [item for item in pred_metric if item in fr_target_names]
+    data_module = TUBDataModule(
+        csv_path,
+        batch_size,
+        data_root_path,
+        pred_metric,
+        transform_list,
+        segments,
+        subsample_percent=data_fraction,
+        match_segments=match_segments,
+    )
+    data_module.setup()
+
+    # here starts newer PTL interface, i think
+    # grab the model class, IE LitWAWEnet2020
+    lightning_module = get_class(
+        lightning_module_name, "wawenet_trainer.lightning_model"
+    )
+    model = lightning_module(
+        initial_learning_rate, num_targets=len(pred_metric), channels=channels
+    )
+
+    # setup callbacks
+    # nothing for now because callbacks aren't ready yet
+
+    # set up a trainer
+    trainer = pl.Trainer(
+        max_epochs=training_epochs,
+        callbacks=[],
+        accelerator="auto",
+        auto_select_gpus=True,
+        auto_scale_batch_size="binsearch",
+        enable_progress_bar=True,
+    )
+
+    if not test_only:
+        trainer.fit(model=model, datamodule=data_module)
+
+    test_output = trainer.test(model=model, datamodule=data_module)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="do a WAWEnets train/test cycle")
     parser.add_argument(
@@ -34,11 +151,7 @@ if __name__ == "__main__":
         type=str,
         help="clearml project to be associated with task",
     )
-    parser.add_argument(
-        "--model_name",
-        type=str,
-        help="name of model to import from `nnate.models`",
-    )
+    # TODO: remove references to old repo code
     parser.add_argument(
         "--dataset_name",
         type=str,
@@ -96,12 +209,12 @@ if __name__ == "__main__":
         "--segments",
         type=str,
         nargs="+",
-        help="names of the 3-second subsegments to include " "when training",
+        help="names of the 3-second subsegments to include when training",
     )
     parser.add_argument(
         "--nisqa_csv_path",
         type=str,
-        help="path pointing to the CSV that contains NISQA-" "style data",
+        help="path pointing to the CSV that contains NISQA-style data",
     )
     parser.add_argument(
         "--data_root_path",
@@ -115,98 +228,32 @@ if __name__ == "__main__":
         default=False,
     )
     parser.add_argument(
-        "--model_class_name",
+        "--lightning_module_name",
         type=str,
         default="",
-        help="the name of the model class that implements pl.LightningModule",
+        help="name of the model class that implements pl.LightningModule",
+    )
+    parser.add_argument(
+        "--initial_learning_rate",
+        type=float,
+        default=None,
+        help="learning rate starting place",
+    )
+    parser.add_argument(
+        "--channels",
+        type=int,
+        default=96,
+        help="number of convolutional channels to be used when training. default "
+        "is 96",
+    )
+    parser.add_argument(
+        "--num_workers",
+        type=int,
+        default=None,
+        help="number of CPU workers that should be used to compute transforms/etc. "
+        "and read data from disk",
     )
 
     args = parser.parse_args()
-
-    target_list = " ".join(args.pred_metric)
-    # TODO: is this fixed yet?
-    Task.add_requirements("setuptools", "59.5.0")
-    task = Task.init(
-        project_name=args.project_name,
-        task_name="_".join(args.pred_metric) + "_from_script",
-        output_uri="/home/whltexbread/ml_results",
-    )
-    # TODO: we've gotta pass the task around so we can upload artifacts
-    #       and some reporting too.
-
-    normalizers = [
-        get_normalizer_class(metric, norm_ind=ind)
-        for ind, metric in enumerate(args.pred_metric)
-    ]
-
-    # set up the dataset
-    speech_transforms = [
-        NormalizeAudio(),
-        AudioToTensor(),
-    ]
-    speech_transforms.extend(normalizers)
-
-    augment_transforms = [
-        NormalizeAudio(),
-        InvertAudioPhase(),
-        AudioToTensor(),
-    ]
-    augment_transforms.extend(normalizers)
-
-    speech_transforms = transforms.Compose(speech_transforms)
-    augment_transforms = transforms.Compose(augment_transforms)
-
-    transform_list = [speech_transforms, augment_transforms]
-    dataset_class = get_class(args.dataset_name, "wawenet_trainer.lightning_data")
-
-    if args.segments:
-        segments = args.segments
-    else:
-        segments = ["seg_1"]
-
-    nisqa_csv_path = args.nisqa_csv_path
-
-    fr_target_names = {
-        "PESQMOSLQO",
-        "POLQAMOSLQO",
-        "PEMO",
-        "ViSQOL3_C310",
-        "STOI",
-        "ESTOI",
-        "SIIBGauss",
-    }
-    match_segments = [item for item in args.pred_metric if item in fr_target_names]
-    data_module = TUBDataModule(
-        nisqa_csv_path,
-        60,
-        "/ml_data/Speech/NISQA_Corpus_raw_16knorm",  # TODO: add to argparse
-        args.pred_metric,
-        transform_list,
-        segments,
-        subsample_percent=args.data_fraction,
-        match_segments=match_segments,
-    )
-    data_module.setup()
-
-    # here starts newer PTL interface, i think
-    # grab the model class, IE LitWAWEnet2020
-    model_class = get_class(args.model_class_name, "wawenet_trainer.lightning_model")
-    # TODO: add learning rate and channels to argparse
-    model = model_class(
-        args.learning_rate, num_targets=len(args.pred_metric), channels=args.channels
-    )
-
-    # setup callbacks
-    # nothing for now because callbacks aren't ready yet
-
-    # set up a trainer
-    trainer = pl.Trainer(
-        max_epochs=args.training_epochs,
-        callbacks=[],
-        accelerator="auto",
-        auto_select_gpus=True,
-        auto_scale_batch_size="binsearch",
-        enable_progress_bar=True,
-    )
-
-    trainer.fit(model=model, datamodule=data_module)
+    # TODO: read configs for specific training regimes so you don't HAVE to deal with
+    #       all these params
