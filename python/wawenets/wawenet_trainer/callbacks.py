414e0f9c1dc9b7012cb9fabad9647e7c8b152210
Andrew Catellier <acatellier@ntia.doc.gov>
2023-01-08 22:45:04 -0700

    small step towards reporting needed artifacts

--- python/wawenets/wawenet_trainer/callbacks.py
+++ python/wawenets/wawenet_trainer/callbacks.py
@@ -54,9 +54,11 @@ def _log_correlations(
     for ind, normalizer in enumerate(pl_module.normalizers):
         pearson_r = _calculate_correlation(y[:, ind], y_hat[:, ind])
         correlations[normalizer.name] = pearson_r
-        pl_module.log(f"{phase} correlation: {normalizer.name}", pearson_r)
+        if "batch" in phase:
+            pl_module.log(f"{phase} correlation: {normalizer.name}", pearson_r)
         all_pearson_r = np.hstack((all_pearson_r, pearson_r))
     mean_pearsor_r = all_pearson_r.mean()
+    pl_module.log(f"{phase} correlation", mean_pearsor_r)
     correlations["mean"] = mean_pearsor_r
     return correlations
 
@@ -64,12 +66,24 @@ def _log_correlations(
 class TestCallbacks(pl.Callback):
     def __init__(self, normalizers: List[NormalizeGenericTarget] = None) -> None:
         self.normalizers = normalizers
-        self.y: list = list()
-        self.y_hat: list = list()
+        # ugh, after all the work i did to be able to log in two places,
+        # i don't see a way around keeping track of predictions and actual values.
+        # this is pretty hairy tbh.
+        self.y_train: list = list()
+        self.y_hat_train: list = list()
+        self.y_val: list = list()
+        self.y_hat_val: list = list()
+        self.y_test: list = list()
+        self.y_hat_test: list = list()
+        self.loss_dict = {
+            "training": list(),
+            "validation": list(),
+        }
+        self.corr_dict = {
+            "training": list(),
+            "validation": list(),
+        }
         super().__init__()
-
-    def _per_target_correlation(self, y: torch.tensor, y_hat: torch.tensor):
-        pass
 
     def on_validation_start(
         self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
@@ -78,6 +92,8 @@ class TestCallbacks(pl.Callback):
         # TODO: make sure tensors don't remain on GPU
         # TODO: handle denormalization here, since this is where we're doing reporting
         # TODO: handle reporting by language, impairment here
+        self.y_val = list()
+        self.y_hat_val = list()
         return super().on_validation_start(trainer, pl_module)
 
     def on_validation_batch_end(
@@ -92,9 +108,20 @@ class TestCallbacks(pl.Callback):
         performance_metrics = _log_performance_metrics(
             outputs, pl_module, "validation batch"
         )
+        self.y_val.append(outputs["y"])
+        self.y_hat_val.append(outputs["y_hat"])
         return super().on_validation_batch_end(
             trainer, pl_module, outputs, batch, batch_idx, dataloader_idx
         )
+
+    def on_validation_epoch_end(
+        self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
+    ) -> None:
+        y = torch.vstack([item for item in self.y_val])
+        y_hat = torch.vstack([item for item in self.y_hat_val])
+        loss = pl_module.loss_fn(y, y_hat)
+        self.loss_dict["validation"][pl_module.current_epoch] = loss
+        return super().on_validation_epoch_end(trainer, pl_module)
 
     def on_validation_end(
         self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
