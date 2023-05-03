58b20878b8265be52036dac1ab45bb0b60d62236
Andrew Catellier <acatellier@ntia.doc.gov>
2023-05-03 09:16:26 -0600

    more docstrings. only the trainer is left, i think.

--- python/wawenets/wawenet_trainer/training_config.py
+++ python/wawenets/wawenet_trainer/training_config.py
@@ -5,6 +5,10 @@ from pkg_resources import resource_filename
 
 import yaml
 
+"""
+handles various tasks related to configuration
+"""
+
 
 def training_params(training_regime: str = "default") -> dict:
     """
@@ -29,14 +33,32 @@ def training_params(training_regime: str = "default") -> dict:
 
 
 # TODO: document this behavior in the training script
-def get_results_path():
+def get_results_path() -> Path:
+    """
+    calculates an absolute path to `~/wenets_training_artifacts`. if the results
+    directory does not exist, it will be created.
+
+    Returns
+    -------
+    Path
+        absolute path to `~/wenets_training_artifacts`
+    """
     user_home_path = Path(os.path.expanduser("~"))
     results_path = user_home_path / "wenets_training_artifacts"
     results_path.mkdir(parents=True, exist_ok=True)
     return results_path
 
 
-def clearml_config_exists():
+def clearml_config_exists() -> bool:
+    """
+    determines whether a clearML configuration exists at the default
+    location used by clearML
+
+    Returns
+    -------
+    bool
+        whether or not a file with the name `~/clearml.conf` exists
+    """
     user_home_path = Path(os.path.expanduser("~"))
     clearml_conf_path = user_home_path / "clearml.conf"
     if clearml_conf_path.exists():
